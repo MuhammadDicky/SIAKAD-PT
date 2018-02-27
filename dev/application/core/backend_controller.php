a21d160fa20da08a6dd522716c3d81189fafc5d1
Muhammad Dicky Hidayat Latif <muh.dickyhidayat@gmail.com>
2018-02-27 08:10:39 +0800

    Clean some line of code

--- dev/application/core/backend_controller.php
+++ dev/application/core/backend_controller.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Backend_Controller extends My_Controller{
 
 	public function __construct(){
-		parent::__construct();		
+		parent::__construct();
 		$this->load->helper(array());
 		$this->load->library(array('form_validation','upload'));
 		$this->load->model(array('fakultas_model','user_admin_model','visitors_logs_model','thn_angkatan_model','ortu_model','ptk_studi_model','ptk_penelitian_model','alumni_model','mahasiswa_do_model'));
