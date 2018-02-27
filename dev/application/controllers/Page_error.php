6dc1c1046ef3f80eae526738dc2c5b17b0f4e489
Muhammad Dicky Hidayat Latif <muh.dickyhidayat@gmail.com>
2018-02-27 08:06:15 +0800

    Add error_500

--- dev/application/controllers/Page_error.php
+++ dev/application/controllers/Page_error.php
@@ -47,6 +47,25 @@ class Page_error extends CI_Controller {
 		}
 	}
 
+	public function error_500(){
+		if (!$this->input->is_ajax_request()) {
+			$this->load->view('errors/bs_error/page_500');
+		}
+		else{
+			$post = $this->input->get(NULL, TRUE);
+			if (isset($post['request_view']) && isset($post['template'])) {
+				$this->load->view(@$post['template'].'views/error_page/page_error_500');
+			}
+			else{
+				$errors = array(
+					'status_page' => 'page_error',
+					'message' => 'Page Error'
+				);
+				echo json_encode($errors);
+			}
+		}
+	}
+
 	public function no_direct_access(){
 		$str = array('direct_access' => 'no_access');
 		$this->load->view('errors/bs_error/page_403',$str);
