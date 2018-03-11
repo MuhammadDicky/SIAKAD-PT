4371cf17fe7cf89a337e3fb6cc7a7bd684f1ff68
Muhammad Dicky Hidayat Latif <muh.dickyhidayat@gmail.com>
2018-03-11 11:20:49 +0800

    Change system path

--- index.php
+++ index.php
@@ -118,7 +118,7 @@ switch (ENVIRONMENT)
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-	$system_path = 'dev/system';
+	$system_path = 'vendor/codeigniter/framework/system';
 
 /*
  *---------------------------------------------------------------
